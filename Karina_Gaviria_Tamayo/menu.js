d213277093588bbb1c4ad09c9323aa72415df028
karinagaviria25 <karina.gaviria@globant.com>
2026-01-24 12:52:52 -0500

    Ejercicio 5: Control de asisitencia y sanción

--- Karina_Gaviria_Tamayo/menu.js
+++ Karina_Gaviria_Tamayo/menu.js
@@ -1,3 +1,6 @@
 import { calcularBilletes } from "./ejercicio1.js";
 import { ajustarPrecios } from "./ejercicio2.js";
 import { evaluarBeca } from "./ejercicio3.js";
+import { calcularTotalCarrito } from "./ejercicio4.js";
+import { evaluarAsistencia } from "./ejercicio5.js";
+
